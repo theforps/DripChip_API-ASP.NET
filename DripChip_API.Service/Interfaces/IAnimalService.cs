280bfa0aef1031ff7ccb4df226da4747448165d1
operator <theforps@gmail.com>
2023-02-14 23:44:30 +0300

    added for logic

---
+++ DripChip_API.Service/Interfaces/IAnimalService.cs
@@ -0,0 +1,9 @@
+﻿using DripChip_API.DAL.Response;
+using DripChip_API.Domain.Models;
+
+namespace DripChip_API.Service.Interfaces;
+
+public interface IAnimalService
+{
+    Task<IBaseResponse<Animal>> GetAnimal(int id);
+}
