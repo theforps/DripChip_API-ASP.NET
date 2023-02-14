e7d462170cdf76b6e5dd3fbc2a40514f23dfb5a3
operator <theforps@gmail.com>
2023-02-14 23:44:00 +0300

    added logic

---
+++ DripChip_API.Service/Implementations/AnimalService.cs
@@ -0,0 +1,51 @@
+﻿using DripChip_API.DAL.Interfaces;
+using DripChip_API.DAL.Response;
+using DripChip_API.Domain.Enums;
+using DripChip_API.Domain.Models;
+using DripChip_API.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DripChip_API.Service.Implementations;
+
+public class AnimalService : IAnimalService
+{
+    private readonly IAnimalRepository _animalRepository;
+
+    public AnimalService(IAnimalRepository animalRepository)
+    {
+        _animalRepository = animalRepository;
+    }
+    
+    public async Task<IBaseResponse<Animal>> GetAnimal(int id)
+    {
+        try
+        {
+            var animal = await _animalRepository.GetAll().FirstOrDefaultAsync(x => x.id == id);
+
+            if (animal == null)
+            {
+                return new BaseResponse<Animal>()
+                {
+                    Description = "Животное не найдено",
+                    StatusCode = StatusCode.AnimalNotFound
+                };
+            }
+            
+            return new BaseResponse<Animal>()
+            {
+                StatusCode = StatusCode.OK,
+                Data = animal
+            };
+        }
+        catch (Exception ex)
+        {
+            return new BaseResponse<Animal>()
+            {
+                Description = $"GetAnimal : {ex.Message}",
+                StatusCode = StatusCode.ServerError,
+            };
+        }
+        
+            
+    }
+}
