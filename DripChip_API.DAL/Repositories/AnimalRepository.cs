7b3541a70fb6c932f77d1da0e44ff5a10923fa11
operator <theforps@gmail.com>
2023-02-14 23:42:47 +0300

    added connect to db

---
+++ DripChip_API.DAL/Repositories/AnimalRepository.cs
@@ -0,0 +1,19 @@
+﻿using DripChip_API.DAL.Interfaces;
+using DripChip_API.Domain.Models;
+
+namespace DripChip_API.DAL.Repositories;
+
+public class AnimalRepository : IAnimalRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public AnimalRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public IQueryable<Animal> GetAll()
+    {
+        return _db.Animals;
+    }
+}
