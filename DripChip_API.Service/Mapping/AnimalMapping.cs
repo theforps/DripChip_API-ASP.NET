37c3432f70970a080ef97d7b417b409536d03c43
operator <theforps@gmail.com>
2023-02-16 19:07:56 +0300

    add mapping for animal

---
+++ DripChip_API.Service/Mapping/AnimalMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DripChip_API.Domain.DTO.Animal;
+using DripChip_API.Domain.Models;
+
+namespace DripChip_API.Service.Mapping;
+
+public class AnimalMapping : Profile
+{
+    public AnimalMapping()
+    {
+        CreateMap<Animal, DTOAnimalSearch>().ReverseMap();
+    }
+}
