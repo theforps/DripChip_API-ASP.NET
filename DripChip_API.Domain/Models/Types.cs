026b3acd9ce03c060ff4a04a439ad016b089e97c
operator <theforps@gmail.com>
2023-02-16 22:14:07 +0300

    add new model for Types of animals

---
+++ DripChip_API.Domain/Models/Types.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DripChip_API.Domain.Models;
+
+public class Types
+{
+    [Key]
+    public long id { get; set; }
+    public string type { get; set; }
+}
