f9fef8bd63c1197b1e518d0de7d9cf5e958a5231
operator <theforps@gmail.com>
2023-02-16 19:08:11 +0300

    add mapping for user

---
+++ DripChip_API.Service/Mapping/UserMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DripChip_API.Domain.DTO;
+using DripChip_API.Domain.Models;
+
+namespace DripChip_API.Service.Mapping;
+
+public class UserMapping : Profile
+{
+    public UserMapping()
+    {
+        CreateMap<User, DTOUserRegistration>().ReverseMap();
+        CreateMap<User, DTOUser>().ReverseMap();
+        CreateMap<User, DTOUserSearch>().ReverseMap();
+    }
+}
