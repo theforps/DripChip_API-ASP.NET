01fd165d16836aa6842d67a17d2d99cc34003a5b
operator <theforps@gmail.com>
2023-02-19 17:13:21 +0300

    add new Model

---
+++ DripChip_API.Domain/Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace DripChip_API.Domain.Models;
+
+public class Location
+{
+    [Key]
+    public long id { get; set; }
+    public double latitude { get; set; }
+    public double longitude { get; set; }
+    [JsonIgnore]
+    public List<LocationInfo> LocationInfo { get; set; }
+}
