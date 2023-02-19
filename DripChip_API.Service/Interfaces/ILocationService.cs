86a9b1424f910197d3525ee34ebcad30b3827812
operator <theforps@gmail.com>
2023-02-19 17:10:13 +0300

    add new func

---
+++ DripChip_API.Service/Interfaces/ILocationService.cs
@@ -0,0 +1,9 @@
+﻿using DripChip_API.DAL.Response;
+using DripChip_API.Domain.Models;
+
+namespace DripChip_API.Service.Interfaces;
+
+public interface ILocationService
+{
+    Task<IBaseResponse<Location>> GetById(long id);
+}
