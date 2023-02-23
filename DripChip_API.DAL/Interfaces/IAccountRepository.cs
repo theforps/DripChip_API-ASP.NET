8742078ab8fce7f8a4327b2caff5f720666fec75
operator <theforps@gmail.com>
2023-02-23 19:47:53 +0300

    all CRUD operations are implemented for users

--- DripChip_API.DAL/Interfaces/IAccountRepository.cs
+++ DripChip_API.DAL/Interfaces/IAccountRepository.cs
@@ -6,4 +6,6 @@ public interface IAccountRepository
 {
     Task<User> GetUserById(int id);
     List<User> GetUsersByParams(User entity, int from, int size);
+    Task<User> UpdateUser(User entity);
+    Task<bool> DeleteUser (int id);
 }
