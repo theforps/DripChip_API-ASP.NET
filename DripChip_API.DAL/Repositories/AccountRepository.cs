186c512b7d46303824ccdd851c817476fa16d0de
operator <theforps@gmail.com>
2023-02-23 19:48:45 +0300

    all CRUD operations are implemented for users

--- DripChip_API.DAL/Repositories/AccountRepository.cs
+++ DripChip_API.DAL/Repositories/AccountRepository.cs
@@ -16,19 +16,44 @@ public class AccountRepository : IAccountRepository
     
     public async Task<User> GetUserById(int id)
     {
-        var result = await _db.Users.FirstOrDefaultAsync(x => x.id == id);
+        var result = await _db.Users.AsNoTracking().FirstOrDefaultAsync(x => x.id == id);
         
         return  result;
     }
     
     public List<User> GetUsersByParams(User userSearch, int from, int size)
     {
-        var result = _db.Users.Where(x =>
+        var result = _db.Users.AsNoTracking().Where(x =>
                 x.firstName.ToLower().Contains(userSearch.firstName.ToLower()) 
                 && x.lastName.ToLower().Contains(userSearch.lastName.ToLower()) 
                 && x.email.ToLower().Contains(userSearch.email.ToLower()))
             .Skip(from).Take(size).OrderBy(x => x.id).ToList();
 
         return result;
+    }
+
+    public async Task<User> UpdateUser(User entity)
+    {
+        _db.Users.Update(entity);
+        await _db.SaveChangesAsync();
+        
+        return entity;
+    }
+
+    public async Task<bool> DeleteUser(int id)
+    {
+        var animals = _db.Animals.AsNoTracking().Where(x => x.chipperId == id);
+
+        if (!animals.Any())
+        {
+            var result = await _db.Users.AsNoTracking().FirstOrDefaultAsync(x => x.id == id);
+
+            _db.Users.Remove(result);
+            await _db.SaveChangesAsync();
+
+            return true;
+        }
+
+        return false;
     }
 }
