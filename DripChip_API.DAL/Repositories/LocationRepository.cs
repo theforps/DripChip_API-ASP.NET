6bb8ae2eec7fc7b1b43a65da0315a15c19581a4b
operator <theforps@gmail.com>
2023-02-26 18:13:23 +0300

    add CRUD

--- DripChip_API.DAL/Repositories/LocationRepository.cs
+++ DripChip_API.DAL/Repositories/LocationRepository.cs
@@ -15,8 +15,60 @@ public class LocationRepository : ILocationRepository
 
     public async Task<Location> GetById(long id)
     {
-        var result = await _db.Locations.FirstOrDefaultAsync(x => x.id == id);
+        var result = await _db.Locations
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.id == id);
+        
+        return result;
+    }
+
+    public async Task<Location> AddLocation(Location location)
+    {
+        await _db.Locations.AddAsync(location);
+        await _db.SaveChangesAsync();
+
+        var result = await _db.Locations.FirstOrDefaultAsync(x => 
+            x.latitude.Equals(location.latitude) && x.longitude.Equals(location.longitude));
+
 
         return result;
+
+    }
+
+    public async Task<bool> CheckExist(Location location)
+    {
+        var result = await _db.Locations
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x =>
+            x.latitude.Equals(location.latitude) && 
+            x.longitude.Equals(location.longitude));
+
+        return result != null;
+    }
+
+    public async Task<Location> UpdateLocation(Location location)
+    {
+        _db.Locations.Update(location);
+        await _db.SaveChangesAsync();
+
+        return location;
+    }
+
+    public async Task<bool> DeleteLocation(long id)
+    {
+        var checkChipLoc = await _db.Animals.Where(x => x.chippingLocationId == id).AnyAsync();
+        var checkVisitedLoc = await _db.LocationInfo.Where(x => x.locationPoint.id == id).AnyAsync();
+
+        if (!checkChipLoc && !checkVisitedLoc)
+        {
+            var entity = await GetById(id);
+            
+            _db.Locations.Remove(entity);
+            await _db.SaveChangesAsync();
+
+            return true;
+        }
+
+        return false;
     }
 }
