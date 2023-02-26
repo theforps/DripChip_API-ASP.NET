3d8cfe534da9c2449ba8892cf2deacfadac45c9a
operator <theforps@gmail.com>
2023-02-26 18:12:16 +0300

    add mapping for Location

---
+++ DripChip_API.Service/Mapping/LocationMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DripChip_API.Domain.DTO.Location;
+using DripChip_API.Domain.Models;
+
+namespace DripChip_API.Service.Mapping;
+
+public class LocationMapping:Profile
+{
+    public LocationMapping()
+    {
+        CreateMap<Location, DTOLocation>().ReverseMap();
+    }
+}
