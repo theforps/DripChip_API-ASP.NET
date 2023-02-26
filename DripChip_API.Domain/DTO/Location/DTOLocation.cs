23b740f94777a4a96161a245de8f24efa289425c
operator <theforps@gmail.com>
2023-02-26 18:11:38 +0300

    add new DTO for model of Location

---
+++ DripChip_API.Domain/DTO/Location/DTOLocation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DripChip_API.Domain.DTO.Location;
+
+public class DTOLocation
+{
+    [Range(-90,90)]
+    [Required]
+    public double latitude { get; set; }
+    [Range(-180,180)]
+    [Required]
+    public double longitude { get; set; }
+}
