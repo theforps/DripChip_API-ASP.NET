8f2cc72f773b016face30b720439362a09f47ae5
operator <theforps@gmail.com>
2023-03-01 21:35:22 +0300

    create mapping configure for register

---
+++ DripChip_API.Service/Mapping/RegisterMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DripChip_API.Domain.DTO;
+using DripChip_API.Domain.Models;
+
+namespace DripChip_API.Service.Mapping;
+
+public class RegisterMapping : Profile
+{
+    public RegisterMapping()
+    {
+        CreateMap<User, DTOUserRegistration>().ReverseMap();
+        CreateMap<User, DTOUser>().ReverseMap();
+    }
+}
