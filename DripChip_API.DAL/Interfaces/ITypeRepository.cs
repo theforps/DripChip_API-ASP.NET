0f824b8dc667cec1acf6791553cd2f57b07f5c69
operator <theforps@gmail.com>
2023-03-01 21:28:49 +0300

    move CRUD from AnimalRepository

---
+++ DripChip_API.DAL/Interfaces/ITypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using DripChip_API.Domain.Models;
+
+namespace DripChip_API.DAL.Interfaces;
+
+public interface ITypeRepository
+{
+    Task<Types> GetTypeById(long id);
+    Task<bool> CheckTypeExist(Types type);
+    Task<Types> AddType(Types type);
+    Task<Types> UpdateType(Types type);
+    Task<bool> DeleteType(long id);
+}
