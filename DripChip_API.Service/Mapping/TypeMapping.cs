68f00db839dc977ba6a57b957e5feb442c4bf1b4
operator <theforps@gmail.com>
2023-03-01 21:34:55 +0300

    create mapping configure for type

---
+++ DripChip_API.Service/Mapping/TypeMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DripChip_API.Domain.DTO.Type;
+using DripChip_API.Domain.Models;
+
+namespace DripChip_API.Service.Mapping;
+
+public class TypeMapping : Profile
+{
+    public TypeMapping()
+    {
+        CreateMap<Types, DTOType>().ReverseMap();
+        CreateMap<Types, DTOTypeInsert>().ReverseMap();
+    }
+}
