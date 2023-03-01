34a5434afae30acd06cbba0c1c736e80353ce094
operator <theforps@gmail.com>
2023-03-01 21:33:02 +0300

    move function for type from AnimalService

---
+++ DripChip_API.Service/Interfaces/ITypeService.cs
@@ -0,0 +1,12 @@
+﻿using DripChip_API.DAL.Response;
+using DripChip_API.Domain.DTO.Type;
+
+namespace DripChip_API.Service.Interfaces;
+
+public interface ITypeService
+{
+    Task<IBaseResponse<DTOType>> GetType(long typeId);
+    Task<IBaseResponse<DTOType>> AddType(DTOTypeInsert type);
+    Task<IBaseResponse<DTOType>> UpdateType(long id, DTOTypeInsert type);
+    Task<IBaseResponse<bool>> DeleteType(long id);
+}
