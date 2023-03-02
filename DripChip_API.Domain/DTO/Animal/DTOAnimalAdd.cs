8fa8250c6b4f07b51572df315ea5bcabf4a4ed2d
operator <theforps@gmail.com>
2023-03-02 20:34:14 +0300

    add DTO model of animal for create

---
+++ DripChip_API.Domain/DTO/Animal/DTOAnimalAdd.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DripChip_API.Domain.DTO.Animal;
+
+public class DTOAnimalAdd
+{
+    [Required] public List<long> animalTypes { get; set; }
+
+    [Required] [Range(1, float.MaxValue)] public float weight { get; set; } 
+
+    [Required] [Range(1, float.MaxValue)] public float length { get; set; } 
+
+    [Required] [Range(1, float.MaxValue)] public float height { get; set; } 
+
+    [Required] public string gender { get; set; } 
+
+    [Required] [Range(1, Int32.MaxValue)] public int chipperId { get; set; } 
+    [Required] [Range(1, long.MaxValue)] public long chippingLocationId { get; set; } 
+}
