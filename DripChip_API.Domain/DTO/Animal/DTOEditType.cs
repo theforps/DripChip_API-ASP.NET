e7b056f7602d189408472da70485ff9bc6b65e35
operator <theforps@gmail.com>
2023-03-09 22:23:51 +0300

    model for changing the type

---
+++ DripChip_API.Domain/DTO/Animal/DTOEditType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DripChip_API.Domain.DTO.Animal;
+
+public class DTOEditType
+{
+    [Required] [Range(1, long.MaxValue)]
+    public long oldTypeId { get; set; }
+    [Required] [Range(1, long.MaxValue)]
+    public long newTypeId { get; set; }
+}
