01497bab95f4843ffb92a59045efb83685dfee8c
operator <theforps@gmail.com>
2023-03-09 22:21:16 +0300

    get query for visited locations of animal

---
+++ DripChip_API.DAL/Interfaces/ILocationInfoRepository.cs
@@ -0,0 +1,8 @@
+﻿using DripChip_API.Domain.Models;
+
+namespace DripChip_API.DAL.Interfaces;
+
+public interface ILocationInfoRepository
+{
+    List<LocationInfo> GetAnimalLocations(long id, int from, int size, DateTime start, DateTime end);
+}
